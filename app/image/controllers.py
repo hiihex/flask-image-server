59d600aa24c04ecf025765002dd3f9b63a6a6b46
kacperslenzak <kacperinos@gmail.com>
2022-01-05 03:17:54 +0000

    Add Upload Image Route

--- app/image/controllers.py
+++ app/image/controllers.py
@@ -1,4 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, current_app, url_for, redirect
+import os
+from werkzeug.utils import secure_filename
 
 image_mod = Blueprint('image_mod', __name__, url_prefix='/image')
 
+
+@image_mod.route('/upload', methods=["POST"])
+def upload_image_post():
+    image_file = request.files.get('image_file')
+    if not image_file:
+        return jsonify({"error": "You did not pass an image in your request"}), 400
+
+    filename = secure_filename(image_file.filename)
+    image_file.save(os.path.join(f"{os.getcwd()}\\app\\static\\upload", filename))
+
+    return jsonify({
+        "status": "success",
+        "image_path": f"http://{request.environ['SERVER_NAME']}:{request.environ['SERVER_PORT']}/static/upload/{filename}"
+    })
--- config.py
+++ config.py
@@ -7,10 +7,7 @@ DEBUG = True
 # Define the application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Define the database - we are working with
-# SQLite for this example
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-DATABASE_CONNECT_OPTIONS = {}
+UPLOAD_FOLDER = '/app/static/uploads'
 
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
