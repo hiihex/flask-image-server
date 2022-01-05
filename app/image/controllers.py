eff6cebfcb4ffee40c919193aea71d4dbcf93d43
kacperslenzak <kacperinos@gmail.com>
2022-01-05 03:26:05 +0000

    Add file verification. Check if file is valid image

--- app/image/controllers.py
+++ app/image/controllers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, current_app, url_for, redirect
 import os
 from werkzeug.utils import secure_filename
+import filetype
 
 image_mod = Blueprint('image_mod', __name__, url_prefix='/image')
 
@@ -10,6 +11,9 @@ def upload_image_post():
     image_file = request.files.get('image_file')
     if not image_file:
         return jsonify({"error": "You did not pass an image in your request"}), 400
+
+    if not filetype.is_image(image_file):
+        return jsonify({"error": "You passed a file that is not an image"}), 400
 
     filename = secure_filename(image_file.filename)
     image_file.save(os.path.join(f"{os.getcwd()}\\app\\static\\upload", filename))
